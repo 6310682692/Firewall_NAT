78cc429462818adfa60ca4676563d0f060a5bb9a
6310682692 <69454013+6310682692@users.noreply.github.com>
2023-10-13 22:23:43 +0700

    Add NAT  func

--- client.py
+++ client.py
@@ -1,26 +1,35 @@
 import socket
 
-# Setup client to connect to server
-server_ip = '192.168.11.147'
-server_port = 8080
+def main():
+    # Setup client to connect to server
+    server_ip = '127.0.0.2'
+    server_port = 8081
 
-# Create a socket object for the client
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Create a socket object for the client
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-client_socket.connect((server_ip, server_port))
-print(f"Connected to {server_ip}:{server_port}")
+    try:
+        # Connect to the server
+        client_socket.connect((server_ip, server_port))
+        print(f"Connected to {server_ip}:{server_port}")
 
-while True:
-    # test sending data
-    message = input("You can now sent message to the server: ")
-    client_socket.send(message.encode('utf-8'))
-    # Check User status
-    if message.upper() == "Q":
-        break
+        while True:
+            # test sending data
+            message = input("You can now sent message to the server: ")
+            client_socket.send(message.encode('utf-8'))
+            # Check User status
+            if message.upper() == "Q":
+                break
 
-# test response
-data = client_socket.recv(1024)
-print(f"Received data from server: {data.decode('utf-8')}")
+        # test response
+        data = client_socket.recv(1024)
+        print(f"Received data from server: {data.decode('utf-8')}")
 
-client_socket.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
--- firewall.py
+++ firewall.py
@@ -1,9 +1,6 @@
 import socket
 
-# Firewall Section
-
-allowed_ip = ["127.0.0.1", "192.168.11.147", "192.168.11.167"]
-denied_ip = ["127.0.0.1"]
+# Firewall Function
 
 def is_allow(ip):
     return ip in allowed_ip
@@ -11,43 +8,88 @@ def is_allow(ip):
 def is_denied(ip):
     return ip in denied_ip
 
-# Setup Socket Server
-server_ip = '192.168.11.147'
-server_port = 8080
+# Socket Init function
+def open_server(gateway_ip, server_port):
+    # Create a socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((gateway_ip, server_port))
+    server_socket.listen(1)
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-# Bind the socket to the server address
-server_socket.bind((server_ip, server_port))
+    print(f"Server is listening on {gateway_ip}:{server_port}")
 
-# Setup connection
-server_socket.listen(1)
+    return server_socket
 
-print(f"Server is listening on {server_ip}:{server_port}")
+# NAT Section
+def NAT_Gate(client_socket, client_address, server_ip, server_port):
+    # Connect nat to the server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect((server_ip, server_port))
 
-# Handle Connection
-while True:
-    # Accept incoming connections
-    client_socket, client_address = server_socket.accept()
-    client_ip = client_address[0]
-
-    # IP testing Here
-    if is_denied(client_ip):
-        print(f"Connection not allowed from {client_ip}:{client_address[1]}")
-        client_socket.close()
-
-    if is_allow(client_ip):
+    # While Connected
+    try:
         while True:
             data = client_socket.recv(1024)
-            if data.decode('utf-8') == 'q':
-                print(f"Client in this ip has leave {client_ip}:{client_address[1]}")
-                client_socket.close()
+
+            if not data:
+                print(f"Client from this IP {client_address} has disconnected")
+                response = "You have been disconnected"
+                client_socket.send(response.encode('utf-8'))
                 break
-            print(f"Data sent from {client_ip}:{client_address[1]}")
-            print(f"Recive data : {data.decode('utf-8')}")
 
-    else:
-        print(f"Connection from unknown IP {client_ip}:{client_address[1]}")
-        client_socket.close()  
+            if (data.decode('utf-8')).upper() == "Q":
+                print(f"Client from this ip {client_address} has disconnected")
+                response = "You have been disconnected"
+                client_socket.send(response.encode('utf-8'))
+                break
 
+            # Sent Client data to server
+            print(f"Data sent from {client_address}")
+            print(f"Received data: {data.decode('utf-8')}")
+            server_socket.send(data)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        client_socket.close()
+        server_socket.close()
+
+def main():
+    while True:
+        # call server function
+        server_socket = open_server(gateway_ip, server_port)
+        try:
+            # After client connect
+                client_socket, client_address = server_socket.accept()
+                client_ip = client_address[0]
+
+                if is_denied(client_ip):
+                    print(f"Connection not allowed from {client_ip}:{client_address[1]}")
+                    response = "Blocked"
+                    client_socket.send(response.encode('utf-8'))
+                    client_socket.close()
+
+                elif is_allow(client_ip):
+                    # Forward traffic through NAT
+                    NAT_Gate(client_socket, client_address, server_ip, server_port)
+
+                else:
+                    print(f"Connection from unknown IP {client_ip}:{client_address[1]}")
+                    response = "Server Said Unknown Client"
+                    client_socket.send(response.encode('utf-8'))
+                    client_socket.close()
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        finally:
+            server_socket.close()
+
+if __name__ == "__main__":
+    # Setup Socket Server
+    server_ip = '127.0.0.2'
+    server_port = 8081
+    allowed_ip = ["127.0.0.1", "192.168.11.147", "192.168.11.167"]
+    denied_ip = [""]
+    gateway_ip = '127.0.0.2' # ip for address translation
+    main()
