25476fcf5050f4a6cfaefe29345110c33bf9a990
Kemika Weerakulwatana <6310682619@student.tu.ac.th>
2023-10-14 21:11:07 +0700

    add logging

--- firewall.py
+++ firewall.py
@@ -1,4 +1,5 @@
 import socket
+import logging
 
 # Firewall Function
 
@@ -49,38 +50,46 @@ def NAT_Gate(client_socket, client_address, server_ip, server_port):
 
     except Exception as e:
         print(f"An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
 
     finally:
         client_socket.close()
         server_socket.close()
 
 def main():
+    # Setup logging
+    logging.basicConfig(filename='firewall_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
     while True:
         # call server function
         server_socket = open_server(gateway_ip, server_port)
         try:
             # After client connect
-                client_socket, client_address = server_socket.accept()
-                client_ip = client_address[0]
+            client_socket, client_address = server_socket.accept()
+            client_ip = client_address[0]
 
-                if is_denied(client_ip):
-                    print(f"Connection not allowed from {client_ip}:{client_address[1]}")
-                    response = "Blocked"
-                    client_socket.send(response.encode('utf-8'))
-                    client_socket.close()
+            if is_denied(client_ip):
+                print(f"Connection not allowed from {client_ip}:{client_address[1]}")
+                response = "Blocked"
+                client_socket.send(response.encode('utf-8'))
+                client_socket.close()
+                logging.info(f"BLOCKED - Connection from {client_ip}:{client_address[1]}")
 
-                elif is_allow(client_ip):
-                    # Forward traffic through NAT
-                    NAT_Gate(client_socket, client_address, server_ip, server_port)
+            elif is_allow(client_ip):
+                # Forward traffic through NAT
+                NAT_Gate(client_socket, client_address, server_ip, server_port)
+                logging.info(f"ALLOWED - Connection from {client_ip}:{client_address[1]}")
 
-                else:
-                    print(f"Connection from unknown IP {client_ip}:{client_address[1]}")
-                    response = "Server Said Unknown Client"
-                    client_socket.send(response.encode('utf-8'))
-                    client_socket.close()
+            else:
+                print(f"Connection from unknown IP {client_ip}:{client_address[1]}")
+                response = "Server Said Unknown Client"
+                client_socket.send(response.encode('utf-8'))
+                client_socket.close()
+                logging.info(f"UNKNOWN - Connection from {client_ip}:{client_address[1]}")
 
         except Exception as e:
             print(f"An error occurred: {e}")
+            logging.error(f"An error occurred: {e}")
 
         finally:
             server_socket.close()
@@ -90,6 +99,6 @@ if __name__ == "__main__":
     server_ip = '127.0.0.2'
     server_port = 8081
     allowed_ip = ["127.0.0.1", "192.168.11.147", "192.168.11.167"]
-    denied_ip = [""]
-    gateway_ip = '127.0.0.2' # ip for address translation
+    denied_ip = [""]  # You might want to add IP addresses to deny in this list
+    gateway_ip = '127.0.0.2'  # ip for address translation
     main()
