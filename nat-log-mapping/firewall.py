ab7cfa211e3c43b84bd293b63de36cb0ce542575
Kemika Weerakulwatana <6310682619@student.tu.ac.th>
2023-11-25 08:58:30 +0700

    log & encrypt data

--- nat-log-mapping/firewall.py
+++ nat-log-mapping/firewall.py
@@ -1,5 +1,7 @@
 import socket
 import logging
+from datetime import datetime
+import secrets
 
 BLOCK_IPS = ['192.168.11.110', '192.168.11.111']
 
@@ -20,61 +22,97 @@ def nat_firewall(client_rec,public_ip, public_port):
     private_ip, private_port = NAT_MAPPING.get((public_ip, public_port), (None, None))
     return private_ip, private_port
 
+DATA_LOG_FILE = 'data_log.txt'
+
+# Log encrypted client data
+def log_encrypted_data(client_ip, encrypted_data, key):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    log_entry = f"{timestamp} Client IP: {client_ip} - {encrypted_data} | {key}\n"
+    with open(DATA_LOG_FILE, 'a') as log_file:
+        log_file.write(log_entry)
+
+# Generate a random key
+def generate_random_key():
+    return secrets.token_bytes(32)
+
+# Encrypt data using XOR
+def encrypt_data(data, key):
+    encrypted_data = bytes([a ^ b for a, b in zip(data.encode(), key)])
+    return encrypted_data
+
 # Main function
 def main():
     # Create a socket object
     firewall_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket
-    firewall_socket.bind(('127.0.0.2', 9090))
+    firewall_port = 9090
+    firewall_socket.bind(('127.0.0.2', firewall_port))
 
     # Listen for incoming connections
     firewall_socket.listen(5)
-    print("Firewall is running and listening on port 9090...")
+
+    print(f"Firewall is running and listening on port {firewall_port}...")
 
     while True:
-        # Accept a new connection
-        client_socket, client_address = firewall_socket.accept()
+        try:
+            # Accept a new connection
+            client_socket, client_address = firewall_socket.accept()
 
-         # Receive client IP, server IP, and server port
-        client_ip, server_ip, server_port = client_socket.recv(1024).decode().strip().split(':')
-        print(f"Connection from {client_ip}")
+            # Receive client IP, server IP, and server port
+            client_ip, server_ip, server_port = client_socket.recv(1024).decode().strip().split(':')
+            print(f"Connection from {client_ip}")
 
-        print(f"Received Client IP: {client_ip}, Server IP: {server_ip}, Server Port: {server_port}")
+            print(f"Received Client IP: {client_ip}, Server IP: {server_ip}, Server Port: {server_port}")
 
-        # Check if the connection is allowed by the NAT firewall
-        private_ip, private_port = nat_firewall(client_ip,server_ip, int(server_port))
-        if private_ip and private_port:
-            print(f"Mapping to Private IP: {private_ip}, Port: {private_port}")
-            client_socket.send(f"Connected to {server_ip}:{server_port}".encode())
+            # Check if the connection is allowed by the NAT firewall
+            private_ip, private_port = nat_firewall(client_ip, server_ip, int(server_port))
+            if private_ip and private_port:
+                print(f"Mapping to Private IP: {private_ip}, Port: {private_port}")
+                client_socket.send(f"Connected to {server_ip}:{server_port}".encode())
 
-            try:
-                while True:
-                    data = client_socket.recv(1024).decode()
-                    if not data:
-                        print(f"Client from {client_ip} has disconnected.")
-                        break
+                try:
+                    # Generate a random key for encryption
+                    encryption_key = generate_random_key()
 
-                    print(f"Received data from client: {data}")
-                    if data.upper() == 'Q':
-                        print(f"Client from {client_ip} has disconnected.")
-                        break
+                    while True:
+                        data = client_socket.recv(1024).decode()
+                        if not data:
+                            print(f"Client from {client_ip} has disconnected.")
+                            break
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
+                        print(f"Received data from client: {data}")
 
-            finally:
+                        # Encrypt the data with the generated key
+                        encrypted_data = encrypt_data(data, encryption_key)
+                        
+                        # Log the encrypted data along with the key
+                        log_encrypted_data(client_ip, encrypted_data.hex(), encryption_key.hex())
+
+                        if data.upper() == 'Q':
+                            print(f"Client from {client_ip} has requested to disconnect.")
+                            break
+
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+
+                finally:
+                    client_socket.close()
+                    print(f"Connection with {client_ip} closed.")
+
+                # Log the allowed connection
+                logging.info(f"ALLOWED - {client_ip} connected to {server_ip}:{server_port} mapped to {private_ip}:{private_port}")
+
+            else:
+                print("Connection blocked by NAT firewall.")
+                client_socket.send("Connection blocked by NAT firewall.".encode())
                 client_socket.close()
+                print(f"Connection with {client_ip} closed.")
+                # Log the blocked connection
+                logging.info(f"BLOCKED - {client_ip}")
 
-            # Log the allowed connection
-            logging.info(f"ALLOWED - {client_ip} connected to {server_ip}:{server_port} mapped to {private_ip}:{private_port}")
-
-        else:
-            print("Connection blocked by NAT firewall.")
-            client_socket.send("Connection blocked by NAT firewall.".encode())
-            client_socket.close()
-            # Log the blocked connection
-            logging.info(f"BLOCKED - {client_ip}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
